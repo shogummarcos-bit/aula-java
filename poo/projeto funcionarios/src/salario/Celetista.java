885171212e351ecdbcc7b5f26e195464aeadc644
shogummarcos-bit <shogummarcos@gmail.com>
2025-10-22 23:22:44 +0000

    projeto funcionario

---
+++ poo/projeto funcionarios/src/salario/Celetista.java
@@ -0,0 +1,6 @@
+class Celetista extends Funcionario{
+    private double fundodegarantia;
+private double ferias;
+private double decimoterceiro;
+private double licencamaternidade;private double licenciapremio;
+}
