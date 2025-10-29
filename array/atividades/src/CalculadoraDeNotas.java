eecef58a95adbc6194d3ab9124a3d96c34c9c232
shogummarcos-bit <shogummarcos@gmail.com>
2025-10-29 00:32:06 +0000

    exercicio 1 array

---
+++ array/atividades/src/CalculadoraDeNotas.java
@@ -0,0 +1,19 @@
+import java.utol.ArrayList;
+import java.util.Scanner;
+public class CalculadoradeNumeros{
+    pubic static vid main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        ArrayList<double>notas=new ArrayList<>();
+        System.out.println("digite a nota dos alunos");
+        while(true){
+            System.out.println("nota:");
+            double nota=sc.nextDouble();
+            if(nota<0){
+                break;
+            }
+            if(notas.isEmpty()){
+
+            }
+        }
+    }
+}
