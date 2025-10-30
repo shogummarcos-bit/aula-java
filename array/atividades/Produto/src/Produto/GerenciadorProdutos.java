c0b42c3774a3dba67b3b4bbf08042714a242efff
shogummarcos-bit <shogummarcos@gmail.com>
2025-10-30 00:29:47 +0000

    exe array produtos

---
+++ array/atividades/Produto/src/Produto/GerenciadorProdutos.java
@@ -0,0 +1,15 @@
+package Produto;
+public class gerenciadorProduto{
+    private List<Produto> produtos;
+}
+public 
+gerenciadorProduto
+(){
+    produtos= new ArraysList<>();
+}
+public void adicionarProduto(Produto produto){
+    produtos.add(produto);
+}
+public boolean removerProduto(String nome){
+    retan produtos.
+}
