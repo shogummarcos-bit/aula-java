abdaf565cd73c5efb6e49720810b40709d93bda7
shogummarcos-bit <shogummarcos@gmail.com>
2025-11-03 23:00:25 +0000

    spring

---
+++ springBoot/springboot/src/main/java/com/exatividade/springboot/controller/ExController.java
@@ -0,0 +1,9 @@
+package com.exatividade.springboot.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/")
+public class ExController {
+
+}
