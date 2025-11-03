4c9cb4ffab9fbb1b7cc7953f17f3db41891f6de7
shogummarcos-bit <shogummarcos@gmail.com>
2025-11-03 23:19:08 +0000

    criação de rotas

--- springBoot/springboot/src/main/java/com/exatividade/springboot/controller/ExController.java
+++ springBoot/springboot/src/main/java/com/exatividade/springboot/controller/ExController.java
@@ -1,9 +1,13 @@
 package com.exatividade.springboot.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/")
 public class ExController {
-
+    @GetMapping("/api")
+    public String home(){
+        return "ola mundo";
+    }
 }
