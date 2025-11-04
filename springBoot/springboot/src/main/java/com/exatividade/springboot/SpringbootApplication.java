7b7483e42a9ff4c54f87b7463ec79a8109e1155d
shogummarcos-bit <shogummarcos@gmail.com>
2025-11-04 00:25:39 +0000

    rotas dimanicas

--- springBoot/springboot/src/main/java/com/exatividade/springboot/SpringbootApplication.java
+++ springBoot/springboot/src/main/java/com/exatividade/springboot/SpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.exatividade.springboot;
+ package com.exatividade.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springBoot/springboot/src/main/java/com/exatividade/springboot/controller/ExController.java
+++ springBoot/springboot/src/main/java/com/exatividade/springboot/controller/ExController.java
@@ -2,6 +2,9 @@ package com.exatividade.springboot.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/")
@@ -10,4 +13,9 @@ public class ExController {
     public String home(){
         return "ola mundo";
     }
+    @GetMapping("/api/test/{numeroUm}")
+    public int teste(@PathVariable int numeroUm){
+        return numeroUm;
+    }
+    
 }
